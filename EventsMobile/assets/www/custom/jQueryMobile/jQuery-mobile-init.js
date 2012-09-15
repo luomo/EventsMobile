5be1c96de97ddb590a9eeff9138358f41409823a
luomo <joaoruicunha@gmail.com>
2012-09-15 11:48:41 +0100

    Events Service Application init. Core project for events restfull web services.

---
+++ EventsMobile/assets/www/custom/jQueryMobile/jQuery-mobile-init.js
@@ -0,0 +1,8 @@
+$( document ).bind( "mobileinit", function() {
+	
+    // Make your jQuery Mobile framework configuration changes here!
+	    console.log("mobileinit");
+    	$.mobile.allowCrossDomainPages = true;
+    	$.mobile.loadingMessage = false;
+
+	});	

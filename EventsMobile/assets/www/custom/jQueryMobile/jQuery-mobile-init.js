d87d058f292dcbfc4fa7724b00a1cac7dc692a80
luomo <joaoruicunha@gmail.com>
2012-09-28 11:04:17 +0100

    Added Page Loader support

--- EventsMobile/assets/www/custom/jQueryMobile/jQuery-mobile-init.js
+++ EventsMobile/assets/www/custom/jQueryMobile/jQuery-mobile-init.js
@@ -3,6 +3,8 @@ $( document ).bind( "mobileinit", function() {
     // Make your jQuery Mobile framework configuration changes here!
 	    console.log("mobileinit");
     	$.mobile.allowCrossDomainPages = true;
-    	$.mobile.loadingMessage = false;
-
+    	//$.mobile.loadingMessage = false;
+    	//$.mobile.loadingMessage = 'Loading ... ';
+    	$.mobile.loadingMessageTextVisible = true;
+    	$.mobile.loadingMessageTheme = 'a';
 	});	

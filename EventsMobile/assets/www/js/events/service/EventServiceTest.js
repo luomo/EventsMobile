5a25f2c3cafc0ab71aaaa98502d59b121100e569
luomo <joaoruicunha@gmail.com>
2012-10-12 12:53:42 +0100

    .

---
+++ EventsMobile/assets/www/js/events/service/EventServiceTest.js
@@ -0,0 +1,149 @@
+// **************************************************************************************************
+// ********************************** Event Singleton application ***********************************
+// **************************************************************************************************
+// creation of a singleton object (as advised in mobile javascript mobile development book)
+// One of the ideas is to create a singleton object that will maintain application state.
+// When we add html5 offline support all the caching mechanism will be placed in this object 
+var EventServiceTest = function () { 
+	
+	
+	function getChanges(syncURL, lastSyncDate, callback){
+		// lastSyncDate will be used to pass the last date to the server
+		// at this point is not beeing used
+    	AjaxEventHelper.createGETRequestAjax(syncURL,callback);
+	}
+	
+	
+	// Public API
+	return {
+		init : function() {
+			alert("EventServiceTest: init ");
+			console.log("EventServiceTest: init ");
+			EventStorageDao.init();
+			EventServiceTest.sync(function(){
+				console.log("nothing to update");
+			})
+		},
+		clear : function(){
+			console.log("EventServiceTest: clear ");
+			EventStorageDao.clear();
+		},
+		createEvent : function (callback, jsonDataToBePosted) {
+			console.log("EventServiceTest: createEvent ");
+			var url = AjaxEventHelper.getRootURL() + 'events';
+			AjaxEventHelper.createPOSTRequestAjax( url, 
+												   function( eventJson ){
+														var event = Event.createEventJSObjectBasedOnJsonAjaxReq(eventJson);
+														EventStorageDao.addOrUpdateEvent(event);			
+														callback(eventJson);
+													},
+													jsonDataToBePosted); 
+		},
+		deleteEventById : function(eventId, callback) {
+			console.log("EventServiceTest: deleteEventById: " + eventId);
+			var url = AjaxEventHelper.getRootURL() + 'events/'+ eventId;
+			AjaxEventHelper.createDELETERequestAjax(url,
+													function ( data ){
+														EventStorageDao.removeEvent(eventId);
+														callback(data)
+													})
+		},
+		getLastSync: function(callback) {
+			EventStorageDao.getLastSyncDate(callback);
+	    },
+	    sync: function(callback) {
+	    	var syncURL = AjaxEventHelper.getRootURL() + 'events';
+	        console.log('Starting synchronization...');
+	        this.getLastSync(
+	        		function(lastSync){
+			            getChanges(syncURL, lastSync, function (data) {
+			            	// if exists new data 
+			        		if(data.length > 0) {
+			        			// we have to update localDB
+			        			var events = [];
+			        			for(var i = 0; i< data.length ; i++) {
+			        			//for(var _eventJson in data) {
+			        				var _eventJson = data[i];
+			        				var _event = Event.createEventJSObjectBasedOnJsonAjaxReq(_eventJson);
+			        				events.push(_event);
+			        			}
+			        			EventStorageDao.syncEventList(events, callback);
+			        		} else {
+			        			// if there are no changes we can apply the callback method 
+			        			callback();
+			        		}
+			            });
+	        });
+	        		
+	    },
+	    findEventsByuserId : function (userId, callback){
+	    	console.log("EventServiceTest:findEventsById: " + userId);
+			EventStorageDao.findEventsByuserId(userId, function(_eventJS) {
+														callback(_eventJS);
+													 });
+	    },
+		findEventById : function (eventId, callback){
+			console.log("EventServiceTest:findEventById: " + eventId);
+			EventStorageDao.findEventById(eventId, function(_eventJS) {
+														callback(_eventJS);
+													 });
+			
+		}, 
+		findEventsforToday : function (date, callback){
+			console.log("EventServiceTest:findEventsforToday: " + date);
+			EventStorageDao.findEventforToday(date, 
+					function(list) {
+						var map = new Object();
+						var _eventJson, _eventJS, city ;
+						for(i=0;i < list.length; i++){
+							_eventJson = list[i];
+							_eventJS = Event.createEventJSObjectBasedOnJsonAjaxReq(jQuery.parseJSON(_eventJson));
+							city = _eventJS.venue.location.city;
+							if(map[city] == undefined ) {
+								map[city] = new Array(); 
+							} 
+							map[city].push(_eventJS);
+						}
+						callback(map);
+					}
+			);
+			
+		}, 
+	    // utility methods for displaying info in the gui
+	    findAllEventsGroupedByCity : function(callback) {
+	    	console.log("EventServiceTest:findAllEventsGroupedByCity");
+			EventStorageDao.findAll( new Date(),  
+							    function(list){
+									var map = new Object();
+									var _eventJson, _eventJS, city ;
+									for(i=0;i < list.length; i++){
+										_eventJson = list[i];
+										_eventJS = Event.createEventJSObjectBasedOnJsonAjaxReq(jQuery.parseJSON(_eventJson));
+										city = _eventJS.venue.location.city;
+										if(map[city] == undefined ) {
+											map[city] = new Array(); 
+										} 
+										map[city].push(_eventJS);
+									}
+									callback(map);
+								}
+			);
+		}, 
+		findEventsByCity : function(cityId, callback){
+			console.log("EventServiceTest:findEventsByCity: " + cityId);
+			EventStorageDao.findEventsByCity( 
+					cityId, 
+					function(jsonEventList) {
+						var jsEventList = new Array();
+						var _eventJson, _eventJS;
+						for(i=0;i < jsonEventList.length; i++){
+							_eventJson = jsonEventList[i];
+							_eventJS = Event.createEventJSObjectBasedOnJsonAjaxReq(jQuery.parseJSON(_eventJson));
+							jsEventList.push(_eventJS);
+						}
+						callback(jsEventList);
+					}); 
+					
+		}
+	}
+}(); 
